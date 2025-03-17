e4590ff1c36fd6b8f8ce91b9c549addd9ab61202
dev-bernardofofg <dev.bernardofofg@gmail.com>
2025-03-17 19:47:01 -0300

    feat: card component and fetching countrys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+'use client'
+
+import { useQuery } from "@tanstack/react-query";
+import BaseCard from "./_components/base-card";
+import { fetchGetAllCountrys } from "./_services/get-all-countrys";
+
 export default function Home() {
-	return <div>Start</div>;
+	const { data } = useQuery({
+		queryKey: ['countrys'],
+		queryFn: () => fetchGetAllCountrys()
+	})
+	return (
+		<section className="max-w-7xl mx-auto w-full pt-24">
+			<div className="grid grid-cols-4 gap-8">
+				{data?.map((country) => (
+					<BaseCard country_flag={country.flags.svg} title={country.name.common} key={country.name.common} />
+				))}
+			</div>
+		</section>
+	);
 }
